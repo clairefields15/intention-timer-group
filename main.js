763adfca7691c8229616672561fa678baecc80f6
Lindsay Schnell <lschnell8@gmail.com>
2021-04-14 19:40:52 -0600

    Begin making functions for activity buttons and add CSS to accomplish the assigned color on click

--- main.js
+++ main.js
@@ -1,37 +1,45 @@
 
-//need event listeners on the three category buttons
-//need event handlers for the buttons- function change color
-  //on click change color of button text, outline and icon
-
 //A user should see an error message if they attempt to submit the
 //form without filling out all fields. (Note: The comp shows the error
 //message for forgetting a description - You should mimic this error
 //messaging for all inputs.)
 
-//need event listener
-
-
-
-
 ////////////////// VARIABLES TO ACCESS DOM ////////////
-var studyButton = document.querySelector('#studyButton');
-var exerciseButton = document.querySelector('#exerciseButton');
-var startButton = document.querySelector('#startActivity');
+var studyButton = document.getElementById('studyButton');
+var exerciseButton = document.getElementById('exerciseButton');
 var meditateButton = document.getElementById('meditateButton');
-var startTimer = document.querySelector('#startButton');
+var startActivityButton = document.getElementById('startActivity');
+var startTimerButton = document.getElementById('startTimer');
 var logActivityButton = document.getElementById('logActivityButton');
 
-
-var currentActivity = ;
+// var currentActivity = ;
 var loggedActivities = [];
 
 ////////// EVENT LISTENERS ///////////////
-studyButton.addEventListener('click', );
-exerciseButton.addEventListener('click', );
-startButton.addEventListener('click', );
-startButton.addEventListener('click', );
-meditateButton.addEventListener('click', );
-logActivityButton.addEventListener('click', );
-
+studyButton.addEventListener('click', onButtonClick);
+meditateButton.addEventListener('click', onButtonClick);
+exerciseButton.addEventListener('click', onButtonClick);
+// startActivityButton.addEventListener('click', );
+// startTimerButton.addEventListener('click', );
+// logActivityButton.addEventListener('click', );
 
 ///////////// EVENT HANDLERS & FUNCTIONS ///////////////
+function onButtonClick() {
+  changeColor();
+}
+
+
+function changeColor(){
+  if (event.target.id === "studyButton") {
+    addColor(studyButton, "study-button-active");
+    removeColor(meditateButton, "meditate-button-active");
+    removeColor(exerciseButton, "exercise-button-active");
+  }
+}
+
+function addColor(button, clickedClass){
+  button.classList.add(clickedClass)
+}
+function removeColor(button, clickedClass){
+  button.classList.remove(clickedClass)
+}
