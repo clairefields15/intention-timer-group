84658521160aa260629247007dc762f550a99d96
Lindsay Schnell <lschnell8@gmail.com>
2021-04-14 18:13:29 -0600

    pseudocode all JS for all iterations

---
+++ main.js
@@ -0,0 +1,34 @@
+//Create variables for DOM interaction by element id
+  //studyButton -claire
+  //meditateButton -lindsay
+  //exerciseButton
+  //startActivity -lindsay
+  //startButton
+  //logActivityButton -lindsay
+
+//need event listeners on the three category buttons
+//need event handlers for the buttons- function change color
+  //on click change color of button text, outline and icon
+
+//A user should see an error message if they attempt to submit the
+//form without filling out all fields. (Note: The comp shows the error
+//message for forgetting a description - You should mimic this error
+//messaging for all inputs.)
+
+//need event listener
+
+
+
+
+////////////////// VARIABLES TO ACCESS DOM ////////////
+var studyButton
+
+
+var currentActivity = ;
+var loggedActivities = [];
+
+////////// EVENT LISTENERS ///////////////
+
+
+
+///////////// EVENT HANDLERS & FUNCTIONS ///////////////
